85c15a276dcf2337b9b976d7b7591055fa4e7bad
Cdogonza <gonzalo.paz@correo.ucu.edu.uy>
2024-11-05 23:11:41 -0300

    updatesssol

--- tarea/backend/routes/correo.Routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-let envio = require('../controllers/correoController');
-
-
-app.post('/enviar', envio.envioCorreo);
-module.exports = app;
